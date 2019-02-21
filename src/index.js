1d06d51bc5a5c9f5056a7541b84a191e16735463
mayuran1096 <ysmayuran11@gmail.com>
2019-02-21 18:47:00 +0530

    initial dildnlklk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+
 import ReactDom from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
+import style from "./style/style.css";
 import VideoList from "./components/video_list";
 import VideoDetails from "./components/video_details";
+import Helmet from "react-helmet";
+
 const API_KEY = "AIzaSyAtuNXcxUIYxFcyIBfu9ENL7COJjqbTnQw";
 
 class App extends Component {
@@ -16,7 +20,10 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="home">
+        <Helmet>
+          <style>{"body { background-color: indianred; }"}</style>
+        </Helmet>
         <SearchBar />
         <VideoDetails video={this.state.selectedItem} />
         <VideoList
